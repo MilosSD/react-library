29f7b4990145dae18ef01a6451bfe51cd0a1f169
Miloš Pirivatrić <pirisd@gmail.com>
2023-11-05 18:52:20 +0100

    added FindByCategory functionality

--- src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -12,19 +12,21 @@ export const SearchBooksPage = () => {
   const [booksPerPage] = useState(5);
   const [totalAmountofBooks, setTotalAmountOfBooks] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
-  const [search, setSearch] = useState('');
-  const[searchUrl, setSearchUrl] = useState('');
+  const [search, setSearch] = useState("");
+  const [searchUrl, setSearchUrl] = useState("");
+  const[categorySelection, setCategorySelection] = useState('Book category');
 
   useEffect(() => {
     const fetchBooks = async () => {
       const baseUrl: string = "http://localhost:8080/api/books";
 
-      let url: string = '';
+      let url: string = "";
 
-      if (searchUrl === ''){
-        url = `${baseUrl}?page=${currentPage - 1 }&size=${booksPerPage}`;
+      if (searchUrl === "") {
+        url = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
       } else {
-        url = baseUrl + searchUrl;
+        let searchWithPage = searchUrl.replace('<pageNumber>', `${currentPage-1}`);
+        url = baseUrl + searchWithPage;
       }
 
       const response = await fetch(url);
@@ -60,7 +62,7 @@ export const SearchBooksPage = () => {
     fetchBooks().catch((error: any) => {
       setIsLoading(false);
       setHttpError(error.message);
-    })
+    });
     window.scrollTo(0, 0);
   }, [currentPage, searchUrl]);
 
@@ -77,10 +79,29 @@ export const SearchBooksPage = () => {
   }
 
   const searchHandleChange = () => {
-    if (search === ''){
-      setSearchUrl('');
+    setCurrentPage(1);
+    if (search === "") {
+      setSearchUrl("");
     } else {
-      setSearchUrl(` /search/findBytitleContaining?title=${search}&page=0&size=${booksPerPage}`)
+      setSearchUrl(
+        ` /search/findBytitleContaining?title=${search}&page=<pageNumber>>&size=${booksPerPage}`);
+    }
+    setCategorySelection('Book category')
+  }
+
+  const categoryField = (value: string) => {
+    setCurrentPage(1);
+    if (
+      value.toLowerCase() === 'fe' ||
+      value.toLowerCase() === 'be'||
+      value.toLowerCase() === 'data' ||
+      value.toLowerCase() === 'devops'
+    ){
+      setCategorySelection(value);
+      setSearchUrl(`/search/findByCategory?category=${value}&page=<pageNumber>>&size=${booksPerPage}`)
+    } else {
+      setCategorySelection('All');
+      setSearchUrl(`?page=<pageNumber>&size=${booksPerPage}`)
     }
   }
 
@@ -105,9 +126,12 @@ export const SearchBooksPage = () => {
                   type="search"
                   placeholder="Search"
                   aria-labelledby="Search"
-                  onChange={e => setSearch(e.target.value)} />
-                <button className="btn btn-outline-success" 
-                  onClick={() => searchHandleChange()}> 
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <button
+                  className="btn btn-outline-success"
+                  onClick={() => searchHandleChange()}
+                >
                   Search
                 </button>
               </div>
@@ -121,33 +145,33 @@ export const SearchBooksPage = () => {
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  Category
+                  {categorySelection}
                 </button>
                 <ul
                   className="dropdown-menu"
                   aria-labelledby="dropdownMenuButton1"
                 >
-                  <li>
+                  <li onClick={() => categoryField('All')}>
                     <a className="dropdown-item" href="#">
                       All
                     </a>
                   </li>
-                  <li>
+                  <li onClick={() => categoryField('FE')}>
                     <a className="dropdown-item" href="#">
                       Front End
                     </a>
                   </li>
-                  <li>
+                  <li onClick={() => categoryField('BE')}>
                     <a className="dropdown-item" href="#">
                       Back End
                     </a>
                   </li>
-                  <li>
+                  <li onClick={() => categoryField('Data')}>
                     <a className="dropdown-item" href="#">
                       Data
                     </a>
                   </li>
-                  <li>
+                  <li onClick={() => categoryField('Devops')}>
                     <a className="dropdown-item" href="#">
                       DevOps
                     </a>
@@ -156,13 +180,30 @@ export const SearchBooksPage = () => {
               </div>
             </div>
           </div>
-          <div className="mt-3">
-            <h5>number of results: ({totalAmountofBooks})</h5>
-          </div>
-          <p>{indexOfFirstBook + 1} to {lastItem} of {indexOfLastBook} items:</p>
-          {books.map((book) => (
-            <SearchBook book={book} key={book.id} />
-          ))}
+          {totalAmountofBooks > 0 ? (
+            <>
+              <div className="mt-3">
+                <h5>number of results: ({totalAmountofBooks})</h5>
+              </div>
+              <p>
+                {indexOfFirstBook + 1} to {lastItem} of {indexOfLastBook} items:
+              </p>
+              {books.map((book) => (
+                <SearchBook book={book} key={book.id} />
+              ))}
+            </>
+          ) : (
+            <div className="m-5">
+              <h3>Can't find what you are looking for?</h3>
+              <a
+                type="button"
+                className="btn main-color btn-md px-4 me-md-2 fm-bold text-white"
+                href="#"
+              >
+                Library Services
+              </a>
+            </div>
+          )}
           {totalPages > 1 && (
             <Pagination
               currentPage={currentPage}
