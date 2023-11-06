51bc4a88354285e8c8c412394e3d6e154c496ffd
Miloš Pirivatrić <pirisd@gmail.com>
2023-11-06 18:16:14 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,25 @@ import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Homepage } from "./layouts/HomePage/Homepage";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 export const App = () => {
   return (
     <div>
       <Navbar/>
-      <Homepage/>
-      <SearchBooksPage/>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home'/>
+        </Route>
+        <Route path='/home'>
+          <Homepage/>
+        </Route>
+        <Route path='/search'>
+          <SearchBooksPage/>
+        </Route>
+      </Switch>
       <Footer/>
+
     </div>
   );
 };
