29baccf8575122ee48161779bd295be0db36ab58
Miloš Pirivatrić <pirisd@gmail.com>
2023-11-09 14:09:13 +0100

    Footer positioning fixed

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,22 @@ import { Redirect, Route, Switch } from "react-router-dom";
 
 export const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/home'/>
-        </Route>
-        <Route path='/home'>
-          <Homepage/>
-        </Route>
-        <Route path='/search'>
-          <SearchBooksPage/>
-        </Route>
-      </Switch>
-      <Footer/>
-
+    <div className="d-flex flex-column min-vh-100 ">
+      <Navbar />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <Homepage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 };
