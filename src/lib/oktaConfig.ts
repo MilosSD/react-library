6b8edf4b4235e01664f9ef3c77fa063100db440a
Miloš Pirivatrić <pirisd@gmail.com>
2023-11-19 13:45:19 +0100

    Auth bug fixing

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,8 +1,8 @@
 export const oktaConfig = {
-    clientId: '0oad8j0cp8d1NLpw75d7',
+    clientId: '0oadcc9kyez5ItUXq5d7',
     issuer: 'https://dev-93883450.okta.com/oauth2/default',
-    redirectUris: 'http://localhost:3000/login/callback',
+    redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true, 
-    disableHttpsCheck: true
+    disableHttpsCheck: true,
 }
