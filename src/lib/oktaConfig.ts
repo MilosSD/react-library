95d869149bd3a1b10dc4faed8baf329f6fceb10b
Miloš Pirivatrić <pirisd@gmail.com>
2023-12-17 15:23:06 +0100

    added HTTPS key and certificate

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oadcc9kyez5ItUXq5d7',
     issuer: 'https://dev-93883450.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true, 
     disableHttpsCheck: true,
