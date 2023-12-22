69b86900e6535f322e995ab2019be930b5b0de02
Miloš Pirivatrić <pirisd@gmail.com>
2023-12-22 18:15:18 +0100

    added Stripe and PaymentInforRequestModel

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_51OPjTsCEuaaoFbDfYBKeVjzVWFsNhH46XdFiDt86mtEK7mWGTNjOclWwnfkDrMoq96vFNDSCzu1EvI6mSgBhADkA00tYUnY5d0');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+  <Elements stripe={stripePromise}>
+  <App />
+  </Elements>
   </BrowserRouter>
 );
 
